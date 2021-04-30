7f37f4664639680c13723d56d57f9722716f15de
shibayu36 <shibayu36@gmail.com>
2021-04-30 22:03:35 +0900

    Mapを使って理想的には早いという気持ちになる

--- 1.two-sum.ts
+++ 1.two-sum.ts
@@ -6,10 +6,20 @@
 
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) return [i, j]
-    }
+  const valToIndex = new Map<number, number>();
+
+  for (let i = 0; i < nums.length; i++) {
+    const pairVal = target - nums[i];
+    const pairIndex = valToIndex.get(pairVal);
+    if (pairIndex !== undefined) return [pairIndex, i];
+
+    valToIndex.set(nums[i], i);
   }
+
+  // for (let i = 0; i < nums.length - 1; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) return [i, j]
+  //   }
+  // }
 };
 // @lc code=end
