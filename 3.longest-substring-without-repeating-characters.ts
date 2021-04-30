7c56980496e1309cfaec69945a2174cdb24f5494
shibayu36 <shibayu36@gmail.com>
2021-05-01 02:16:37 +0900

    longest-substring: startポイントを決めて実装する

--- 3.longest-substring-without-repeating-characters.ts
+++ 3.longest-substring-without-repeating-characters.ts
@@ -6,33 +6,26 @@
 
 // @lc code=start
 function lengthOfLongestSubstring(s: string): number {
-  /**
-   * {
-   *   "a": [0, 3],
-   *   "b": [1, 4, 6, 7],
-   *   "c": [2, 5],
-   * }
-   */
-  const charToIndices: { [key: string]: number[] } = {};
+  let substrStart = 0;
+  let maxLength = 0;
+  let charToPos: Record<string, number> = {};
 
   for (let i = 0; i < s.length; i++) {
     const char = s.charAt(i);
-    charToIndices[char] ??= [];
-    charToIndices[char].push(i);
+
+    const prevPosition = charToPos[char];
+    if (prevPosition !== undefined && prevPosition >= substrStart) {
+      // 現在のsubstr内で重複が見つかれば、最初の文字から現在までが最も長い
+      const currentLen = i - substrStart;
+      if (currentLen > maxLength) maxLength = currentLen;
+      substrStart = prevPosition + 1;
+    }
+
+    charToPos[char] = i;
   }
 
-  console.log(charToIndices);
-
-  let maxLength = 0;
-  for (const char of Object.keys(charToIndices)) {
-    const indices = charToIndices[char];
-    indices.push(s.length);
-    for (let i = 0; i < indices.length - 1; i++) {
-      const len = indices[i + 1] - indices[i];
-      if (len > maxLength) {
-        maxLength = len;
-      }
-    }
+  if (s.length - substrStart > maxLength) {
+    maxLength = s.length - substrStart;
   }
 
   return maxLength;
