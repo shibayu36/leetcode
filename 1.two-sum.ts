82ab277b52645b4b5b39fb3c3ef2089bc753e8ac
shibayu36 <shibayu36@gmail.com>
2021-04-30 21:55:20 +0900

    two-sum

---
+++ 1.two-sum.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j]
+    }
+  }
+};
+// @lc code=end
