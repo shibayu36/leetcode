5911b3c055c7a5137a6595461b909c28323d42a8
shibayu36 <shibayu36@gmail.com>
2021-05-01 23:18:02 +0900

    簡易的なテスト

--- 1.two-sum.ts
+++ 1.two-sum.ts
@@ -16,6 +16,8 @@ function twoSum(nums: number[], target: number): number[] {
     valToIndex[nums[i]] = i;
   }
 
+  return [];
+
   // for (let i = 0; i < nums.length - 1; i++) {
   //   for (let j = i + 1; j < nums.length; j++) {
   //     if (nums[i] + nums[j] === target) return [i, j]
