5d078e17e2fa085d8000bb7928ca93de1a89b81d
shibayu36 <shibayu36@gmail.com>
2021-05-06 23:52:08 +0900

    一旦acceptはされるようになった

--- 5.longest-palindromic-substring.test.ts
+++ 5.longest-palindromic-substring.test.ts
@@ -2,10 +2,12 @@ import { isPalindrome } from "./5.longest-palindromic-substring";
 
 describe("isPalindrome", () => {
   it.each(["", "あ", "あいいあ", "たけやぶやけた", "回文回"])("回文である: %s", (s) => {
-    expect(isPalindrome(s, 0, s.length - 1)).toBe(true);
+    const cache = Array.from(new Array(s.length), () => new Array(s.length).fill(undefined));
+    expect(isPalindrome(s, 0, s.length - 1, cache)).toBe(true);
   });
 
   it.each(["あい", "あいう"])("回文でない: %s", (s) => {
-    expect(isPalindrome(s, 0, s.length - 1)).toBe(false);
+    const cache = Array.from(new Array(s.length), () => new Array(s.length).fill(undefined));
+    expect(isPalindrome(s, 0, s.length - 1, cache)).toBe(false);
   });
 });
--- 5.longest-palindromic-substring.ts
+++ 5.longest-palindromic-substring.ts
@@ -5,12 +5,28 @@
  */
 
 // @lc code=start
-function longestPalindrome(s: string): string {}
+function longestPalindrome(s: string): string {
+  const cache = Array.from(new Array(s.length), () => new Array(s.length).fill(undefined));
 
-function isPalindrome(s: string, start: number, end: number): boolean {
+  for (let len = s.length; len > 0; len--) {
+    for (let start = 0; start + len <= s.length; start++) {
+      if (isPalindrome(s, start, start + len - 1, cache)) {
+        return s.substring(start, start + len);
+      }
+    }
+  }
+  return "";
+}
+
+type PalindromeCache = (boolean | undefined)[][];
+function isPalindrome(s: string, start: number, end: number, cache: PalindromeCache): boolean {
   // 0文字または1文字なら回文
   if (start >= end) return true;
 
-  return s.charAt(start) === s.charAt(end) && isPalindrome(s, start + 1, end - 1);
+  if (cache[start][end] !== undefined) {
+    return cache[start][end] as boolean;
+  }
+
+  return (cache[start][end] = s.charAt(start) === s.charAt(end) && isPalindrome(s, start + 1, end - 1, cache));
 }
 // @lc code=end
