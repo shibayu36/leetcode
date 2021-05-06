e2bad30ffb744a606ecd0ae5ff3fb6c059fd0dd9
shibayu36 <shibayu36@gmail.com>
2021-05-06 21:51:08 +0900

    ひとまず回文判定を用意しようとしている

--- 5.longest-palindromic-substring.ts
+++ 5.longest-palindromic-substring.ts
@@ -6,4 +6,11 @@
 
 // @lc code=start
 function longestPalindrome(s: string): string {}
+
+function isPalindrome(s: string, start: number, end: number): boolean {
+  // 0文字または1文字なら回文
+  if (start >= end) return true;
+
+  return s.charAt(start) === s.charAt(end) && isPalindrome(s, start + 1, end - 1);
+}
 // @lc code=end
