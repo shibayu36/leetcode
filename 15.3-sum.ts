6f0429acfb8f5309edd846260091a237de718fc1
shibayu36 <shibayu36@gmail.com>
2021-05-10 13:39:46 +0900

    まずは基本の総当たり法だが、これはduplicate tripletsが入っててだめ

---
+++ 15.3-sum.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=15 lang=typescript
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+function threeSum(nums: number[]): number[][] {
+  const answers: number[][] = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === 0) answers.push([nums[i], nums[j], nums[k]]);
+      }
+    }
+  }
+
+  return answers;
+}
+// @lc code=end
