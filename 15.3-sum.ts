2aa8c71b0d189cb0ce7ca48cb536b8f21a18d522
shibayu36 <shibayu36@gmail.com>
2021-05-11 09:53:06 +0900

    色々ミスってた...

--- 15.3-sum.ts
+++ 15.3-sum.ts
@@ -8,7 +8,7 @@
 function threeSum(nums: number[]): number[][] {
   const numToCount = new Map<number, number>();
   for (let i = 0; i < nums.length; i++) {
-    numToCount.set(nums[i], numToCount.get(nums[i]) ?? 0 + 1);
+    numToCount.set(nums[i], (numToCount.get(nums[i]) ?? 0) + 1);
   }
 
   const answers: number[][] = [];
@@ -23,8 +23,8 @@ function threeSum(nums: number[]): number[][] {
 
   for (let i = 0; i < positives.length; i++) {
     for (let j = 0; j < negatives.length; j++) {
-      const num1 = candidateNums[i];
-      const num2 = candidateNums[j];
+      const num1 = positives[i];
+      const num2 = negatives[j];
       const rest = -(num1 + num2);
       if (numToCount.get(rest) === undefined) continue;
       if ((num1 === rest && (numToCount.get(num1) ?? 0) < 2) || (num2 === rest && (numToCount.get(num2) ?? 0) < 2)) {
