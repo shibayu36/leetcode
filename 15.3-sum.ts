8fdd82253ed82b4f8985c9b7482bb7de8baf9530
shibayu36 <shibayu36@gmail.com>
2021-05-11 10:02:59 +0900

    わかりやすくする

--- 15.3-sum.ts
+++ 15.3-sum.ts
@@ -12,29 +12,34 @@ function threeSum(nums: number[]): number[][] {
   }
 
   const answers: number[][] = [];
+
+  // 足して0になるということは必ずpositiveとnegativeが一つずつ含まれているので
+  // その二つを使ってループする。ただし0が三つのケースを漏らしてしまうので
+  // 例外的に処理をする
   const zeroCount = numToCount.get(0);
   if (zeroCount !== undefined && zeroCount >= 3) {
     answers.push([0, 0, 0]);
   }
-
   const candidateNums = [...numToCount.keys()];
   const positives = candidateNums.filter((n) => n > 0);
   const negatives = candidateNums.filter((n) => n < 0);
-
   for (let i = 0; i < positives.length; i++) {
     for (let j = 0; j < negatives.length; j++) {
       const num1 = positives[i];
       const num2 = negatives[j];
       const rest = -(num1 + num2);
+      // 条件を満たすrestが存在するかチェック
       if (numToCount.get(rest) === undefined) continue;
+      // restがあったとしても、num1もしくはnum2と同じ値だった場合
+      // 要素数が足りているかチェックする
       if ((num1 === rest && (numToCount.get(num1) ?? 0) < 2) || (num2 === rest && (numToCount.get(num2) ?? 0) < 2)) {
         continue;
       }
-      answers.push([num1, num2, rest].sort((a, b) => a - b));
+      answers.push([num1, num2, rest]);
     }
   }
 
-  return uniqBy(answers, (a) => a.join("-"));
+  return uniqBy(answers, (a) => a.sort((a, b) => a - b).join("-"));
 }
 
 function uniqBy<T>(array: T[], func: (item: T) => unknown) {
