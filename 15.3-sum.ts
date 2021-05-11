6efa58e88f369ff7ff618847c89d0165ec69dcf1
shibayu36 <shibayu36@gmail.com>
2021-05-11 09:38:52 +0900

    uniqはまだできてないけどそれっぽくなった

--- 15.3-sum.ts
+++ 15.3-sum.ts
@@ -6,13 +6,31 @@
 
 // @lc code=start
 function threeSum(nums: number[]): number[][] {
-  const answers: number[][] = [];
+  const numToCount = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    numToCount.set(nums[i], numToCount.get(nums[i]) ?? 0 + 1);
+  }
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i + 1; j < nums.length - 1; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        if (nums[i] + nums[j] + nums[k] === 0) answers.push([nums[i], nums[j], nums[k]]);
+  const answers: number[][] = [];
+  const zeroCount = numToCount.get(0);
+  if (zeroCount !== undefined && zeroCount >= 3) {
+    answers.push([0, 0, 0]);
+  }
+
+  const candidateNums = [...numToCount.keys()];
+  const positives = candidateNums.filter((n) => n > 0);
+  const negatives = candidateNums.filter((n) => n < 0);
+
+  for (let i = 0; i < positives.length; i++) {
+    for (let j = 0; j < negatives.length; j++) {
+      const num1 = candidateNums[i];
+      const num2 = candidateNums[j];
+      const rest = -(num1 + num2);
+      if (numToCount.get(rest) === undefined) continue;
+      if ((num1 === rest && (numToCount.get(num1) ?? 0) < 2) || (num2 === rest && (numToCount.get(num2) ?? 0) < 2)) {
+        continue;
       }
+      answers.push([num1, num2, rest].sort((a, b) => a - b));
     }
   }
 
