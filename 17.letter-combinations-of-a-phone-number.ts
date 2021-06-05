27535a0eac2990bf3dc45dca7267529387b161dd
shibayu36 <shibayu36@gmail.com>
2021-06-05 14:45:18 +0900

    とりあえずめちゃ遅いけどaccept

--- 17.letter-combinations-of-a-phone-number.ts
+++ 17.letter-combinations-of-a-phone-number.ts
@@ -5,8 +5,35 @@
  */
 
 // @lc code=start
-function letterCombinations(digits: string): string[] {
-
+const digitToNumbers: { [key: string]: string[] } = {
+  "2": ["a", "b", "c"],
+  "3": ["d", "e", "f"],
+  "4": ["g", "h", "i"],
+  "5": ["j", "k", "l"],
+  "6": ["m", "n", "o"],
+  "7": ["p", "q", "r", "s"],
+  "8": ["t", "u", "v"],
+  "9": ["w", "x", "y", "z"],
 };
-// @lc code=end
 
+function letterCombinations(digits: string): string[] {
+  return _letterCombinations(digits, 0);
+}
+
+function _letterCombinations(digits: string, index: number): string[] {
+  if (digits.length === 0) return [];
+
+  if (index + 1 === digits.length) {
+    return digitToNumbers[digits.charAt(index)];
+  }
+
+  const result: string[] = [];
+  const restCombinations = _letterCombinations(digits, index + 1);
+  for (const letter of digitToNumbers[digits.charAt(index)]) {
+    for (const combination of restCombinations) {
+      result.push(letter + combination);
+    }
+  }
+  return result;
+}
+// @lc code=end
