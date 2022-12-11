4ca97534b8b1cfc397ce455cb12d3b85057ce49f
shibayu36 <shibayu36@gmail.com>
2022-12-11 16:12:40 +0900

    First answer. of 1.two-sum with ruby

---
+++ 1.two-sum.rb
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=1 lang=ruby
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  num2index = nums.map.with_index { |num, index| [num, index] }.to_h
+
+  nums.each_with_index do |num, index|
+    another = target - num
+    if (index2 = num2index[another]) && (index != index2)
+      return [index, index2]
+    end
+  end
+end
+# @lc code=end
