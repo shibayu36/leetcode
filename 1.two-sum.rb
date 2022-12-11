e522596c17542e84dc992cd5f91a8799dd66b91d
shibayu36 <shibayu36@gmail.com>
2022-12-11 16:15:00 +0900

    Second answer of 1.two-sum with ruby, seeing discussions
    * My first code has 2 loops on nums
    * The second code has 1 loop on nums

--- 1.two-sum.rb
+++ 1.two-sum.rb
@@ -9,13 +9,22 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-  num2index = nums.map.with_index { |num, index| [num, index] }.to_h
-
+  num2index = {}
   nums.each_with_index do |num, index|
-    another = target - num
-    if (index2 = num2index[another]) && (index != index2)
-      return [index, index2]
-    end
+    index2 = num2index[target - num]
+    return [index, index2] if index2
+
+    num2index[num] = index
   end
 end
+# def two_sum(nums, target)
+#   num2index = nums.map.with_index { |num, index| [num, index] }.to_h
+
+#   nums.each_with_index do |num, index|
+#     another = target - num
+#     if (index2 = num2index[another]) && (index != index2)
+#       return [index, index2]
+#     end
+#   end
+# end
 # @lc code=end
