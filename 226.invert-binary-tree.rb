97b28d9f102ae78f508faa09fdf8be345817e370
shibayu36 <shibayu36@gmail.com>
2023-02-12 14:49:52 +0900

    Solve by breadth-first search

--- 226.invert-binary-tree.rb
+++ 226.invert-binary-tree.rb
@@ -17,14 +17,27 @@
 # @param {TreeNode} root
 # @return {TreeNode}
 def invert_tree(root)
-  return root unless root
-  return root if root.left.nil? && root.right.nil?
+  queue = []
+  queue.push(root) unless root.nil?
 
-  left = invert_tree(root.left)
-  right = invert_tree(root.right)
-  root.left = right
-  root.right = left
+  while queue.size > 0
+    current = queue.shift
+    queue.push(current.left) if current.left
+    queue.push(current.right) if current.right
+    current.left, current.right = current.right, current.left
+  end
 
   root
 end
+# def invert_tree(root)
+#   return root unless root
+#   return root if root.left.nil? && root.right.nil?
+
+#   left = invert_tree(root.left)
+#   right = invert_tree(root.right)
+#   root.left = right
+#   root.right = left
+
+#   root
+# end
 # @lc code=end
