f69081c19439d2293ef187b19022b3b862c8a6fa
shibayu36 <shibayu36@gmail.com>
2023-02-15 19:34:47 +0900

    Solve permutations

---
+++ 46.permutations.rb
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=46 lang=ruby
+#
+# [46] Permutations
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute(nums)
+  return [nums] if nums.size <= 1
+
+  result = []
+  nums.each do |num|
+    rest = nums - [num]
+    permute(rest).each do |perm|
+      result << ([num] + perm)
+    end
+  end
+
+  result
+end
+# @lc code=end
+
+p(permute([1]))
+p(permute([1, 2]))
+p(permute([1, 2, 3]))
