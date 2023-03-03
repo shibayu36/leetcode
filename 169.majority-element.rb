48bed860029d00ce926ec9ba293acad213a70865
shibayu36 <shibayu36@gmail.com>
2023-03-03 19:49:45 +0900

    O(1) memory solution

--- 169.majority-element.rb
+++ 169.majority-element.rb
@@ -8,10 +8,26 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def majority_element(nums)
-  hash = Hash.new(0)
+  count = 0
+  major = nil
   nums.each do |num|
-    hash[num] += 1
+    if count == 0
+      count += 1
+      major = num
+    elsif major == num
+      count += 1
+    else
+      count -= 1
+    end
   end
-  hash.keys.max_by { |k| hash[k] }
+  major
 end
 # @lc code=end
+
+# def majority_element(nums)
+#   hash = Hash.new(0)
+#   nums.each do |num|
+#     hash[num] += 1
+#   end
+#   hash.keys.max_by { |k| hash[k] }
+# end
