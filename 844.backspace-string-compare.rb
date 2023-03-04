03f801c1266c5afc77ba4174ec5e9cf8423cf1ea
shibayu36 <shibayu36@gmail.com>
2023-03-04 16:19:06 +0900

    Make backspace_compare simple by using lambda

--- 844.backspace-string-compare.rb
+++ 844.backspace-string-compare.rb
@@ -9,24 +9,40 @@
 # @param {String} t
 # @return {Boolean}
 def backspace_compare(s, t)
-  s_res = ''
-  s.chars.each do |c|
-    if c == '#'
-      s_res.chop!
-    else
-      s_res += c
+  construct = lambda do |string|
+    res = ''
+    string.chars.each do |c|
+      if c == '#'
+        res.chop!
+      else
+        res += c
+      end
     end
+    res
   end
 
-  t_res = ''
-  t.chars.each do |c|
-    if c == '#'
-      t_res.chop!
-    else
-      t_res += c
-    end
-  end
-
-  s_res == t_res
+  construct.call(s) == construct.call(t)
 end
 # @lc code=end
+
+# def backspace_compare(s, t)
+#   s_res = ''
+#   s.chars.each do |c|
+#     if c == '#'
+#       s_res.chop!
+#     else
+#       s_res += c
+#     end
+#   end
+
+#   t_res = ''
+#   t.chars.each do |c|
+#     if c == '#'
+#       t_res.chop!
+#     else
+#       t_res += c
+#     end
+#   end
+
+#   s_res == t_res
+# end
