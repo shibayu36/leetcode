c2ec2745389bae3c5d13e096b11386874c03e414
shibayu36 <shibayu36@gmail.com>
2023-03-05 12:03:41 +0900

    Solve longest-substring-without-repeating-characters

---
+++ 3.longest-substring-without-repeating-characters.rb
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=3 lang=ruby
+#
+# [3] Longest Substring Without Repeating Characters
+#
+
+# @lc code=start
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+  start = 0
+  finish = 0
+
+  longest = 0
+  location = {} # char => index
+  while finish < s.length
+    if location.include?(s[finish])
+      current_length = finish - start
+      longest = [longest, current_length].max
+
+      start = location[s[finish]] + 1
+      location = (start..finish).map { |i| [s[i], i] }.to_h
+    else
+      location[s[finish]] = finish
+    end
+
+    finish += 1
+  end
+
+  last_length = finish - start
+  [longest, last_length].max
+end
+# @lc code=end
+
+p(length_of_longest_substring('abcabcbb'))
+p(length_of_longest_substring('pwwkew'))
+p(length_of_longest_substring('abba'))
