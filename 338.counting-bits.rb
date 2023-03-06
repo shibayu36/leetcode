dc1fd5fc7a6e3b27939e95b860d72409394afd71
shibayu36 <shibayu36@gmail.com>
2023-03-06 21:16:46 +0900

    dp solution

--- 338.counting-bits.rb
+++ 338.counting-bits.rb
@@ -8,8 +8,22 @@
 # @param {Integer} n
 # @return {Integer[]}
 def count_bits(n)
-  (0..n).map do |i|
-    i.to_s(2).count('1')
+  return [0] if n == 0
+
+  dp = [0, 1]
+  (2..n).map do |i|
+    if i.even?
+      dp[i] = dp[i / 2]
+    else
+      dp[i] = dp[i / 2] + 1
+    end
   end
+  dp
 end
 # @lc code=end
+
+# def count_bits(n)
+#   (0..n).map do |i|
+#     i.to_s(2).count('1')
+#   end
+# end
