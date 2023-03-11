4f01424bcd6d9859ccaee273f1d5ddb8d3492dd3
shibayu36 <shibayu36@gmail.com>
2023-03-11 17:18:25 +0900

    Optimize memory, but it is not meaningful because ruby itself already optimized

--- 28.find-the-index-of-the-first-occurrence-in-a-string.rb
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.rb
@@ -12,11 +12,24 @@ def str_str(haystack, needle)
   return -1 if haystack.length < needle.length
 
   (0..haystack.length - needle.length).each do |i|
-    if haystack[i..i + needle.length - 1] == needle
-      return i
+    (0..needle.length - 1).each do |j|
+      break if haystack[i + j] != needle[j]
+      return i if j == needle.length - 1
     end
   end
 
   -1
 end
 # @lc code=end
+
+# def str_str(haystack, needle)
+#   return -1 if haystack.length < needle.length
+
+#   (0..haystack.length - needle.length).each do |i|
+#     if haystack[i..i + needle.length - 1] == needle
+#       return i
+#     end
+#   end
+
+#   -1
+# end
