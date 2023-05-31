19ac0cd7bc859e697a39bedd66450ac307626ec3
shibayu36 <shibayu36@gmail.com>
2023-05-31 15:45:17 +0900

    Update

--- 283.move-zeroes.rb
+++ 283.move-zeroes.rb
@@ -29,6 +29,27 @@ def move_zeroes(nums)
 end
 # @lc code=end
 
+# def move_zeroes(nums)
+#   zero_pos = 0
+#   rest_pos = 0
+
+#   while zero_pos < nums.length
+#     if nums[zero_pos] == 0
+#       rest_pos = zero_pos + 1 if rest_pos <= zero_pos
+
+#       while nums[rest_pos] == 0
+#         rest_pos += 1
+#       end
+
+#       return if rest_pos >= nums.length
+
+#       nums[zero_pos], nums[rest_pos] = nums[rest_pos], nums[zero_pos]
+#     end
+
+#     zero_pos += 1
+#   end
+# end
+
 nums = [1, 0]
 move_zeroes(nums)
 p(nums)
