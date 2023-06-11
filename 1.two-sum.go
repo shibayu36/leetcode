b98ec18c235369d34cc6e06e32e70e79f23f8d41
shibayu36 <shibayu36@gmail.com>
2023-06-11 13:14:09 +0900

    Refactor

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -7,20 +7,37 @@ package main
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
-	val2index := make(map[int]int, len(nums))
-	for i, val := range nums {
-		val2index[val] = i
-	}
+	visitedNumbers := make(map[int]int)
 
-	for i, val := range nums {
-		pair_i, ok := val2index[target-val]
+	for i := 0; i < len(nums); i++ {
+		current := nums[i]
+		pair := target - current
 
-		if ok && i != pair_i {
-			return []int{i, pair_i}
+		if pairIndex, ok := visitedNumbers[pair]; ok {
+			return []int{i, pairIndex}
 		}
+
+		visitedNumbers[current] = i
 	}
 
 	return nil
 }
+
+// func twoSum(nums []int, target int) []int {
+// 	val2index := make(map[int]int, len(nums))
+// 	for i, val := range nums {
+// 		val2index[val] = i
+// 	}
+
+// 	for i, val := range nums {
+// 		pair_i, ok := val2index[target-val]
+
+// 		if ok && i != pair_i {
+// 			return []int{i, pair_i}
+// 		}
+// 	}
+
+// 	return nil
+// }
 
 // @lc code=end
