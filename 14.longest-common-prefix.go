b3d07c37c29c28aad881e9d6d42a36285ed6c6be
shibayu36 <shibayu36@gmail.com>
2023-06-11 18:15:37 +0900

    Refactor

--- 14.longest-common-prefix.go
+++ 14.longest-common-prefix.go
@@ -9,25 +9,38 @@ package main
 import "fmt"
 
 // @lc code=start
-func longestCommonPrefix(strs []string) string {
-	var prefix string
+// func longestCommonPrefix(strs []string) string {
+// 	var prefix string
 
-	for i := 0; i < len(strs[0]); i++ {
-		cToCompare := strs[0][i]
-		for j := 1; j < len(strs); j++ {
-			targetStr := strs[j]
-			if i > len(targetStr)-1 {
-				return prefix
+// 	for i := 0; i < len(strs[0]); i++ {
+// 		cToCompare := strs[0][i]
+// 		for j := 1; j < len(strs); j++ {
+// 			targetStr := strs[j]
+// 			if i > len(targetStr)-1 {
+// 				return prefix
+// 			}
+// 			if targetStr[i] != cToCompare {
+// 				return prefix
+// 			}
+// 		}
+
+// 		prefix += string(cToCompare)
+// 	}
+
+// 	return prefix
+// }
+
+func longestCommonPrefix(strs []string) string {
+	for i := 0; ; i++ {
+		for _, str := range strs {
+			if len(str) == i {
+				return str[:i]
 			}
-			if targetStr[i] != cToCompare {
-				return prefix
+			if str[i] != strs[0][i] {
+				return str[:i]
 			}
 		}
-
-		prefix += string(cToCompare)
 	}
-
-	return prefix
 }
 
 // @lc code=end
