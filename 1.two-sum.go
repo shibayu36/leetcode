66c573001c38bb2201fdc867be27f8009711f5e2
shibayu36 <shibayu36@gmail.com>
2023-06-11 13:07:18 +0900

    Solve two-sum.go

---
+++ 1.two-sum.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ */
+package main
+
+// @lc code=start
+func twoSum(nums []int, target int) []int {
+	val2index := make(map[int]int, len(nums))
+	for i, val := range nums {
+		val2index[val] = i
+	}
+
+	for i, val := range nums {
+		pair_i, ok := val2index[target-val]
+
+		if ok && i != pair_i {
+			return []int{i, pair_i}
+		}
+	}
+
+	return nil
+}
+
+// @lc code=end
