103b4e5e961a5d984d08b867665c56b9aadd3908
shibayu36 <shibayu36@gmail.com>
2023-06-12 16:44:46 +0900

    Solve 121.best-time-to-buy-and-sell-stock.go

---
+++ 121.best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=121 lang=golang
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+package main
+
+// @lc code=start
+func maxProfit(prices []int) int {
+	minPrice := 100000
+	maxProfit := 0
+
+	for i := 1; i <= len(prices); i++ {
+		price := prices[i-1]
+		if price < minPrice {
+			minPrice = price
+			continue
+		}
+
+		profit := price - minPrice
+		if profit > maxProfit {
+			maxProfit = profit
+		}
+	}
+
+	return maxProfit
+}
+
+// @lc code=end
