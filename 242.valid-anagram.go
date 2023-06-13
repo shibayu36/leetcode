92bfa2309a9d25810fd0bd5b1ad51d4609d108cb
shibayu36 <shibayu36@gmail.com>
2023-06-13 11:08:08 +0900

    Optimize memory

--- 242.valid-anagram.go
+++ 242.valid-anagram.go
@@ -11,14 +11,14 @@ func isAnagram(s string, t string) bool {
 		return false
 	}
 
-	exists := make(map[byte]int)
+	count := [26]int{}
 
 	for i := 0; i < len(s); i++ {
-		exists[s[i]]++
-		exists[t[i]]--
+		count[s[i]-'a']++
+		count[t[i]-'a']--
 	}
 
-	for _, val := range exists {
+	for _, val := range count {
 		if val != 0 {
 			return false
 		}
@@ -26,5 +26,26 @@ func isAnagram(s string, t string) bool {
 
 	return true
 }
+
+// func isAnagram(s string, t string) bool {
+// 	if len(s) != len(t) {
+// 		return false
+// 	}
+
+// 	exists := make(map[byte]int)
+
+// 	for i := 0; i < len(s); i++ {
+// 		exists[s[i]]++
+// 		exists[t[i]]--
+// 	}
+
+// 	for _, val := range exists {
+// 		if val != 0 {
+// 			return false
+// 		}
+// 	}
+
+// 	return true
+// }
 
 // @lc code=end
